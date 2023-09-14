456fe0f955919776c0f1dcb87daf8e51a9e179ee
bharatchandran <bharatchandrankr@gmail.com>
2023-09-14 22:24:46 +0530

    modified stying for addMembers page

--- src/components/AddMemberList/AddMemberList.jsx
+++ src/components/AddMemberList/AddMemberList.jsx
@@ -9,16 +9,18 @@ export default function AddMemberList({users, projectId, setAddMemberComplete, a
          return <AddMemberListItem user={user} projectId={projectId} setAddMemberComplete={setAddMemberComplete} addMemberCompelete={addMemberCompelete}/>
         })
     return(
-        <Card className="w-7/12 min-h-screen  ">
+        <div className="w-full h-screen flex justify-center items-center">
+        <Card className="w-7/12 h-4/5   ">
             <CardHeader className="flex justify-center mt-10 mb-10 text-4xl">
                 Add Members
             </CardHeader>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center overflow-auto">
 
             {addMemberList}
             
            
             </div>
         </Card>
+        </div>
     )
 }
