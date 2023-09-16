c0f28ec93882fbe3f8eba1475159b388b9fb363b
bharatchandran <bharatchandrankr@gmail.com>
2023-09-16 08:13:24 +0530

    cleaned projectDetail.jsx

--- src/pages/ProjectDetail/ProjectDetail.jsx
+++ src/pages/ProjectDetail/ProjectDetail.jsx
@@ -1,10 +1,5 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import * as commitAPI from "../../utilities/commit-api"
-import CommitList from "../../components/CommitList/CommitList"
 import Commit from "../Commit/Commit"
 export default function ProjectDetail() {
-   
     return(<div>
         <Commit />
     </div>)
